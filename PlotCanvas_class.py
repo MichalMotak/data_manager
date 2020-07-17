33fdd9318d79d028c0361123e02614a94667c743
MichalMotak <michal.motak1@gmail.com>
2020-07-17 18:05:19 +0200

    Updated PlotCanvas_class.py

--- PlotCanvas_class.py
+++ PlotCanvas_class.py
@@ -1,6 +1,6 @@
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
+import seaborn as sns
 
 class PlotCanvas(FigureCanvas):
 
@@ -24,21 +24,150 @@ class PlotCanvas(FigureCanvas):
         #                            QSizePolicy.Expanding)
         # FigureCanvas.updateGeometry(self)
 
-        print(self.table.table_test)
-        print(self.table)
 
 
-    def plot(self):
-        print('plot')
-        print(self.table)
-        print('data shape', self.table.data.shape)
-        self.ax.plot(self.table.data[:12,1], self.table.data[:12,2])
-        self.draw()
+    def plot_indexes(self, t, x, y, h):
+
+        print('plot3')
+        self.ax.clear()
+        print(type(int(x)))
 
 
-    def plot2(self, cols):
-        print('plot2')
-        print(cols)
-        print('data shape', self.table.data.shape)
-        self.ax.plot(self.table.data[:12,cols[0]], self.table.data[:12,cols[1]])
-        self.draw()
+        # Dla jednego y
+        # if t == 'Line plot':
+        #     self.ax.plot(self.table.data[:50, int(x)], self.table.data[:50, int(y)])
+        # elif t =='Bar plot':
+        #     self.ax.bar(self.table.data[:50, int(x)], self.table.data[:50, int(y)])
+        # elif t =='Scatter plot':
+        #     self.ax.scatter(self.table.data[:50, int(x)], self.table.data[:50, int(y)])
+        # else:
+        #     None
+        c = self.table.col_labels.tolist()
+        ys = y.split(',')
+        xs = x.split(',')
+        hs = h.split(',')
+
+        # ..............................................
+        labels = []
+        if len(xs) == 1:
+            if h:
+                for y in ys:
+                    if t == 'Line plot':
+                        sns.lineplot(x=c[int(x)], y=c[int(y)],hue = c[int(h)], data=self.table.dataframe, ax=self.ax)
+                    elif t == 'Bar plot':
+                        sns.barplot(x = c[int(x)], y = c[int(y)],hue = c[int(h)], data = self.table.dataframe, ax = self.ax)
+                    elif t =='Scatter plot':
+                        sns.scatterplot(x = c[int(x)], y = c[int(y)],hue = c[int(h)], data = self.table.dataframe, ax = self.ax)
+                    else:
+                        None
+                    labels.append(c[int(y)])
+
+
+            elif not h:
+                for y in ys:
+                    if t == 'Line plot':
+                        sns.lineplot(x=c[int(x)], y=c[int(y)], data=self.table.dataframe, ax=self.ax)
+                    elif t == 'Bar plot':
+                        sns.barplot(x = c[int(x)], y = c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    elif t =='Scatter plot':
+                        sns.scatterplot(x = c[int(x)], y = c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    else:
+                        None
+                    labels.append(c[int(y)])
+
+        # if len(xs) == 1:
+        #     if len(ys) == 2:
+        #         print('2')
+        #         if t == 'Line plot':
+        #             sns.lineplot(x=c[int(x)], y=c[int(ys[0])],  data=self.table.dataframe, ax=self.ax)
+        #             self.ax2 = self.ax.twinx()
+        #             sns.lineplot(x=c[int(x)], y=c[int(ys[1])], data=self.table.dataframe, ax=self.ax2)
+        #         elif t == 'Bar plot':
+        #             sns.barplot(x=c[int(x)], y=c[int(y)], data=self.table.dataframe, ax=self.ax)
+        #         elif t == 'Scatter plot':
+        #             sns.scatterplot(x=c[int(x)], y=c[int(y)], data=self.table.dataframe,ax=self.ax)
+        #         else:
+        #             None
+        #         # self.ax = self.ax.twinx()
+
+
+
+            print(labels)
+            self.ax.set_xlabel(c[int(x)])
+            self.ax.set_ylabel(c[int(y)])
+            # self.ax.legend(labels = labels)
+            self.ax.legend()
+            self.fig.tight_layout()
+            self.draw()
+
+
+    def plot_col_names(self, t, x, y, h):
+
+        print('plot4')
+        self.ax.clear()
+
+        c = self.table.col_labels.tolist()
+        ys = y.split(',')
+        xs = x.split(',')
+        hs = h.split(',')
+        xs = [c.index(i) for i in xs]
+        ys = [c.index(i) for i in ys]
+        xs2 = xs[0]
+        print(xs, ys)
+
+
+        # ..............................................
+        labels = []
+        if len(xs) == 1:
+            if h:
+                print('h')
+                for y in ys:
+                    if t == 'Line plot':
+                        sns.lineplot(x=c[int(xs2)], y=c[int(y)], hue = c[int(h)], data=self.table.dataframe, ax=self.ax)
+                    elif t == 'Bar plot':
+                        sns.barplot(x=c[int(xs2)], y=c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    elif t =='Scatter plot':
+                        sns.scatterplot(x=c[int(xs2)], y=c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    else:
+                        None
+                    # labels.append(c[int(y)])
+
+
+            elif not h:
+                print('not h')
+                for y in ys:
+                    if t == 'Line plot':
+                        print(xs2, y)
+                        sns.lineplot(x=c[int(xs2)], y=c[int(y)], data=self.table.dataframe, ax=self.ax)
+                    elif t == 'Bar plot':
+                        sns.barplot(x=c[int(xs2)], y=c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    elif t =='Scatter plot':
+                        sns.scatterplot(x=c[int(xs2)], y=c[int(y)], data = self.table.dataframe, ax = self.ax)
+                    else:
+                        None
+                    # labels.append(c[int(y)])
+
+        # if len(xs) == 1:
+        #     if len(ys) == 2:
+        #         print('2')
+        #         if t == 'Line plot':
+        #             sns.lineplot(x=c[int(x)], y=c[int(ys[0])],  data=self.table.dataframe, ax=self.ax)
+        #             self.ax2 = self.ax.twinx()
+        #             sns.lineplot(x=c[int(x)], y=c[int(ys[1])], data=self.table.dataframe, ax=self.ax2)
+        #         elif t == 'Bar plot':
+        #             sns.barplot(x=c[int(x)], y=c[int(y)], data=self.table.dataframe, ax=self.ax)
+        #         elif t == 'Scatter plot':
+        #             sns.scatterplot(x=c[int(x)], y=c[int(y)], data=self.table.dataframe,ax=self.ax)
+        #         else:
+        #             None
+        #         # self.ax = self.ax.twinx()
+
+
+
+            print(labels)
+            # self.ax.set_xlabel(c[int(x)])
+            # self.ax.set_ylabel(c[int(y)])
+            # self.ax.legend(labels = labels)
+            self.ax.legend()
+            self.fig.tight_layout()
+            self.draw()
